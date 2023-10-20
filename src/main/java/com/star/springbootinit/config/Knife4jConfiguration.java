fcbd909ecb87fd879a65dda78599892cfc1691d9
star <1792945133@qq.com>
2023-10-20 18:45:24 +0800

    项目初始化，并集成Swagger文档

---
+++ src/main/java/com/star/springbootinit/config/Knife4jConfiguration.java
@@ -0,0 +1,36 @@
+package com.star.springbootinit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+
+/**
+ * Knife4j 接口文档配置
+ * https://doc.xiaominfo.com/docs/action/springboot
+ */
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfiguration {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("脚手架文档接口")
+                        .description("Swagger-bootstrap-ui-文档 RESTful APIs")
+                        .termsOfServiceUrl("https://www.staro.cc/")
+                        .version("1.0")
+                        .build())
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.star.springbootinit.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
---
+++ src/main/java/com/star/springbootinit/tools/MybatisInit.java
@@ -0,0 +1,14 @@
+package com.star.springbootinit.tools;
+
+/**
+ * <p>
+ *      Mybatis 自动生成配置文档
+ *      文档：https://mybatis.org/generator/configreference/xmlconfig.html
+ * </p>
+ *
+ * @author: Star
+ * @Description: com.star.springbootinit.tools
+ * @Version: 1.0
+ */
+public class MybatisInit {
+}
