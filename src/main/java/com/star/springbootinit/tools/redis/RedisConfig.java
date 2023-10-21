b2d4b5e0c87972b0354d24b334f5d5035d866d8f
star <1792945133@qq.com>
2023-10-21 14:52:08 +0800

    redis工具类编写完成

---
+++ src/main/java/com/star/springbootinit/tools/redis/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.star.springbootinit.tools.redis;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ *  Redis配置类
+ * </p>
+ *
+ * @author: Star
+ * @Description: com.star.staroj.utils.redis
+ * @Version: 1.0
+ */
+@EnableCaching
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+
+}
