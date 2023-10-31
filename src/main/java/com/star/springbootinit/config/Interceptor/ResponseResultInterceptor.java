4a4364e24b990516837f65def9722fd103e1cde2
star <1792945133@qq.com>
2023-10-31 16:34:18 +0800

    整合Cglib实现类的拷贝复制

---
+++ src/main/java/com/star/springbootinit/config/Interceptor/ResponseResultInterceptor.java
@@ -0,0 +1,52 @@
+package com.star.springbootinit.config.Interceptor;
+
+import com.star.springbootinit.common.resp.ResponseResult;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+/**
+ * @Auther: Star
+ * @Date: 2023-06-14
+ * @Description: com.staro.train.common.Interceptor 响应结果拦截器
+ * @Version: 1.0
+ */
+@Component
+public class ResponseResultInterceptor implements HandlerInterceptor {
+    /* 使用统一返回体的标识 */
+    private static final String RESPONSE_RESULT_ANNOTATION = "RESPONSE-RESULT-ANNOTATION";
+
+    /**
+     * 前处理 拦截器 该方法业务逻辑主要是确定返回给前端的数据是否标注了注解  ResponseResult 如果标注了则给请求体内增加 RESPONSE_RESULT_ANNOTATION
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 正在处理请求的方法bean
+        if (handler instanceof HandlerMethod) {
+            final HandlerMethod handlerMethod = (HandlerMethod) handler;
+            // 获取当前类
+            final Class<?> clazz = handlerMethod.getBeanType();
+            // 获取当前方法
+            final Method method = handlerMethod.getMethod();
+            // 判断是否在类对象上加了注解
+            if (clazz.isAnnotationPresent(ResponseResult.class)) {
+                // 设置该请求返回体，需要包装，往下传递，在ResponseBodyAdvice接口进行判断 存入全局
+                request.setAttribute(RESPONSE_RESULT_ANNOTATION, clazz.getAnnotation(ResponseResult.class));
+            }
+            // 判断是否在方法上加了注解
+            else if (method.isAnnotationPresent(ResponseResult.class)) {
+                // 设置该请求返回体，需要包装，往下传递，在ResponseBodyAdvice接口进行判断
+                request.setAttribute(RESPONSE_RESULT_ANNOTATION, method.getAnnotation(ResponseResult.class));
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/star/springbootinit/config/ResponseAdvice.java
@@ -0,0 +1,69 @@
+package com.star.springbootinit.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.star.springbootinit.common.resp.R;
+import com.star.springbootinit.common.resp.ResponseResult;
+import com.star.springbootinit.exception.BaseException;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ * @Auther: Star
+ * @Description:  全局统一返回
+ * @Version: 1.0
+ */
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+    @Resource
+    private ObjectMapper objectMapper;
+    /* 使用统一返回体的标识 */
+    private static final String RESPONSE_RESULT_ANNOTATION = "RESPONSE-RESULT-ANNOTATION";
+
+    /**
+     * 如果为 false 则不进行统一返回 则不知 beforeBodyWrite
+     * @param methodParameter 方法参数
+     * @param aClass 类
+     * @return
+     */
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(sra).getRequest();
+        ResponseResult responseResult = (ResponseResult) request.getAttribute(RESPONSE_RESULT_ANNOTATION);
+        // 判断返回体是否需要处理
+        return responseResult != null;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        // 提供一定的灵活度，如果body已经被包装了，就不进行包装
+        if (body instanceof R) {
+            return body;
+        }
+        // 异常响应体则直接返回code+message的消息体
+        if (body instanceof BaseException) {
+            return body;
+        }
+        // 如果返回的是String类型 会进行转换错误，所以需要特意适配
+        if (body instanceof String) {
+            try {
+                return this.objectMapper.writeValueAsString(R.success(body));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return R.success(body);
+    }
+}
