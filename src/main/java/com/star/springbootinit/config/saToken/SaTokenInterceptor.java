9f871656a491721c7ab0fcb8b76247a3ab6fc8ed
star <1792945133@qq.com>
2023-11-02 09:52:31 +0800

    1、权限拦截器判断的异常返回，排除注销需要的权限 2、增加token前缀Bearer和token临时有效期，如果在一段时间内没有操作及过期

--- src/main/java/com/star/springbootinit/config/saToken/SaTokenInterceptor.java
+++ src/main/java/com/star/springbootinit/config/saToken/SaTokenInterceptor.java
@@ -1,9 +1,12 @@
 package com.star.springbootinit.config.saToken;
 
 
+import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
+import com.star.springbootinit.common.resp.ResultCode;
+import com.star.springbootinit.exception.BizException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -34,15 +37,21 @@ public class SaTokenInterceptor implements WebMvcConfigurer {
                             .match("/**")    // 拦截的 path 列表，可以写多个 */
                             // 排除掉的 path 列表
                             // 排除指定url  获取的方法
-                            .notMatch("/**/login/**", "/**/logout/**", "/**/error/**",
+                            .notMatch("/**/login/**", "/**/outLogin/**", "/**/error/**",
                                     "/**/register/**", "/**/verify/**", "/**/monitorLogin/**", "/**/enterprise/get",
                                     "/**/enterprise/getLogo", "/**/getSAStoken/**", "/**/favicon.ico")
                             .notMatch("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**", "/doc.html")
                             .check(r -> {
                                         // 先判断是否登录
-                                        StpUtil.checkLogin();
+                                        try {
+                                            StpUtil.checkLogin();
+                                        } catch (NotLoginException e) {
+                                            log.error(e.getMessage());
+                                            throw new BizException(ResultCode.RC403);
+                                        }
                                         // 查看token是否要过期
-                                        long tokenSessionTimeout = StpUtil.getTokenSessionTimeout();// 获取当前 Token-Session 剩余有效时间 (单位: 秒)
+                                        long tokenSessionTimeout = StpUtil.getTokenTimeout();// 获取当前 Token 剩余有效时间 (单位: 秒)
+                                        log.info("tokenSessionTimeout剩余时间{}:", tokenSessionTimeout);
                                         // 如果小于一天 那么就进行续期
                                         if (tokenSessionTimeout < (60 * 60 * 24)) {
                                             // 续期10天
@@ -51,7 +60,7 @@ public class SaTokenInterceptor implements WebMvcConfigurer {
                                     }
                             );        // 校验是否登录了
                     // 根据路由划分模块，不同模块不同鉴权
-                    SaRouter.notMatch("/**/login/**").match("/user/**", r -> StpUtil.checkPermission("user"));
+                    SaRouter.notMatch("/**/login/**","/**/outLogin/**").match("/user/**", r -> StpUtil.checkPermission("user"));
                     SaRouter.match("/role/**", r -> StpUtil.checkPermission("role"));
                     SaRouter.match("/func/**", r -> StpUtil.checkPermission("func"));
                 }))
